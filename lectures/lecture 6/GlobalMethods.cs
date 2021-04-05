2b0e2ea8b7c5d75281ffdbc864da9c9c977d43f1
Furkan Gözükara <furkangozukara@gmail.com>
2021-04-06 01:17:50 +0300

    lecture 6

---
+++ lectures/lecture 6/GlobalMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_6
+{
+    public static class GlobalMethods
+    {
+        private static string ComputeSha256Hash(this string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string returnUserPw(string srUserPw, string srUserFirstName)
+        {
+            //no rainbow tables will be useful with salted encyrpted password
+            return ComputeSha256Hash(srUserFirstName + "$½£" + srUserPw);
+        }
+    }
+}
