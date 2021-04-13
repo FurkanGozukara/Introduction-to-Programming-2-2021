d44a61d63df8ad921dc1b984fd5ddc18d3875265
Furkan Gözükara <furkangozukara@gmail.com>
2021-04-13 04:08:12 +0300

    lecture 7 and semester project upload

---
+++ lectures/lecture 7/CustomClasses/LoginOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lecture_6.CustomClasses
+{
+    public static class LoginOperations
+    {
+        public static TblUsers loggedUser = null;
+        public static DateTime dtLoginTime;
+
+        public static void loginTry(MainWindow main)
+        {
+            using (PMSContext context = new PMSContext())
+            {
+                var vrIpTestResult = context.TblLoginTry.Where(pr => pr.TriedIp == GlobalMethods.returnUserIp() & pr.DateOfTry.AddMinutes(15) > DateTimeHelper.ServerTime).Count();
+
+                if (vrIpTestResult >= 5)
+                {
+                    MessageBox.Show("Error! You have so many times tried to login withing 15 minutes. Try again later!");
+                    return;
+                }
+
+                var vrUserEmail = context.TblUsers.FirstOrDefault(pr => pr.UserEmail == main.txtLoginEmail.Text);
+
+                if (vrUserEmail == null)
+                {
+                    increaseLoginTry();
+                    MessageBox.Show("Error! No such user is found!");
+                    return;
+                }
+
+                if (vrUserEmail.UserPw != GlobalMethods.returnUserPw(main.txtLoginPassword.Password.ToString(), vrUserEmail.SaltOfPw))
+                {
+                    increaseLoginTry();
+                    MessageBox.Show("Error! The entered password is incorrect");
+                    return;
+                }
+
+                loggedUser = vrUserEmail;
+                loggedUser.UserPw = null;//you can nullfiy the sensitive information so they wont remain in ram memory
+                dtLoginTime = DateTimeHelper.ServerTime;
+
+                GlobalMethods.changeLoginStatus();
+                MessageBox.Show("You have successfully logged-in");
+            }
+        }
+
+        private static void increaseLoginTry()
+        {
+            using (PMSContext context = new PMSContext())
+            {
+                TblLoginTry tempTry = new TblLoginTry();
+                tempTry.TriedIp = GlobalMethods.returnUserIp();
+                tempTry.DateOfTry = DateTimeHelper.ServerTime;
+                context.TblLoginTry.Add(tempTry);
+                context.SaveChanges();
+            }
+        }
+
+        public static void tryLogout()
+        {
+            loggedUser = null;
+            GlobalMethods.changeLoginStatus();
+            MessageBox.Show("You have successfully logged-out");
+        }
+    }
+}
---
+++ lectures/lecture 7/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using lecture_6.CustomClasses;
+
+namespace lecture_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GlobalMethods.main = this;
+            AppInit.initApp(this);
+            GlobalMethods.startInfoOperations();
+        }     
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterOperations.completeRegister(this);
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginOperations.loginTry(this);
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            LoginOperations.tryLogout();
+        }
+    }
+}
